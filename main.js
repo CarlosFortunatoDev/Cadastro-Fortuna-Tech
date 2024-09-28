457018390cc05994c28d52a8a8073a2f15b21764
Carlos Fortunato <carlosfortunatodev@gmail.com>
2024-09-28 17:39:48 -0300

    Adicionando lógica para calculo automático no campo Salário Contabilizado

--- main.js
+++ main.js
@@ -3,15 +3,29 @@ const buttonReset = document.querySelector("#reset")
 const salarioBase = document.querySelector("#salarioBase")
 const horasTrabalhadas = document.querySelector("#horasTrabalhadas")
 const horaExtra = document.querySelector("#horaExtra")
+const salarioTotal = document.querySelector("#salarioTotal")
+const calcular = document.querySelector("#calcular")
 
 
 
+document.addEventListener('DOMContentLoaded', () => {
+
+  const calcularSalario = () =>{
+    const valorSalarioBase = parseFloat(salarioBase.value) || 0;
+    const ValorHorasTrabalhadas = parseFloat(horasTrabalhadas.value) || 0;
+    const ValorHoraExtra = parseFloat(horaExtra.value) || 0;
+
+    const valorSalarioTotal = ((valorSalarioBase / 240) * ValorHorasTrabalhadas) + ((valorSalarioBase / 120) * ValorHoraExtra)
+    salarioTotal.value = valorSalarioTotal.toFixed(2)
+  }
+  salarioBase.addEventListener('input', calcularSalario);
+  horasTrabalhadas.addEventListener('input', calcularSalario);
+  horaExtra.addEventListener('input', calcularSalario);
+
+})
 
 
 buttonReset.addEventListener("click", () => {
-  console.log(salarioBase.value);
-  console.log(diasTrabalhados.value);
-  console.log(horaExtra.value);
   for (let i = 0; i < formulario.length; i++) {
     formulario[i].value = ''
   }
