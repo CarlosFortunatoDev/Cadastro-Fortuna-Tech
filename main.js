26f8a16b0acf095510d2ce2f174c62192e78619a
Carlos Fortunato <carlosfortunatodev@gmail.com>
2024-10-19 16:56:16 -0300

    Últimos ajustes e adição de jornada de trabalho já fazendo o cálculo correto no Salário contabilizado

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const formulario = document.querySelector("#formulario")
 const nomeCompleto = document.querySelector("#fullName")
 const buttonReset = document.querySelector("#reset")
 const salarioBase = document.querySelector("#salarioBase")
+const jornadaDeTrabalho = document.querySelector("#jornadaDeTrabalho")
 const horasTrabalhadas = document.querySelector("#horasTrabalhadas")
 const horaExtra = document.querySelector("#horaExtra")
 const salarioTotal = document.querySelector("#salarioTotal")
@@ -13,13 +14,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const calcularSalario = () =>{
     const valorSalarioBase = parseFloat(salarioBase.value) || 0;
+    const valorJornadaDeTrabalho = parseFloat(jornadaDeTrabalho.value) || 0;
     const ValorHorasTrabalhadas = parseFloat(horasTrabalhadas.value) || 0;
     const ValorHoraExtra = parseFloat(horaExtra.value) || 0;
 
-    const valorSalarioTotal = ((valorSalarioBase / 240) * ValorHorasTrabalhadas) + ((valorSalarioBase / 120) * ValorHoraExtra)
-    salarioTotal.value = valorSalarioTotal.toFixed(2)
+    if (valorJornadaDeTrabalho == 44){
+      const valorSalarioTotal = ((valorSalarioBase / 176) * ValorHorasTrabalhadas) + ((valorSalarioBase / 120) * ValorHoraExtra)
+      salarioTotal.value = valorSalarioTotal.toFixed(2)
+    } else if(valorJornadaDeTrabalho == 30){
+      const valorSalarioTotal = ((valorSalarioBase / 120) * ValorHorasTrabalhadas) + ((valorSalarioBase / 120) * ValorHoraExtra)
+      salarioTotal.value = valorSalarioTotal.toFixed(2)
+    }
   }
   salarioBase.addEventListener('input', calcularSalario);
+  jornadaDeTrabalho.addEventListener('input', calcularSalario);
   horasTrabalhadas.addEventListener('input', calcularSalario);
   horaExtra.addEventListener('input', calcularSalario);
 
